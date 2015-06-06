823ff833e1322c2f82028c0cd26bedb2c4a92669
ProgramFOX <programfox@hotmail.be>
2015-06-06 14:12:12 +0200

    Add module infrastructure

---
+++ antispam.js
@@ -0,0 +1,21 @@
+module.exports = {};
+
+module.exports.isSpam = function() {
+    return false;
+};
+
+module.exports.registerFileGood = function(filename) {
+};
+
+module.exports.registerFileBad = function(filename) {
+};
+
+module.exports.registerStringGood = function(str) {
+};
+
+module.exports.registerStringBad = function(str) {
+};
+
+module.exports.truncateKeyword = function(keyword) {
+};
+
