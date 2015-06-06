c9832438355d5e29366c64433a9e635fffb4ac08
ProgramFOX <programfox@hotmail.be>
2015-06-06 18:27:53 +0200

    Add dbQuery method

--- antispam.js
+++ antispam.js
@@ -1,6 +1,23 @@
 var fs = require('fs');
+var pg = require('pg');
 
 module.exports = {};
+
+module.exports.postgreConnStr = '';
+module.exports.tableName = 'antispam';
+
+function dbQuery(query, qParams, callback) {
+    pg.connect(module.exports.postgreConnStr, function(err, client, done) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        client.query(query, qParams, function(err, result) {
+            done();
+            callback(err, result);
+        });
+    });
+}
 
 module.exports.isSpamFile = function(filename) {
     return module.exports.isSpamString(fs.readFileSync(filename, {encoding:'utf8'}));
